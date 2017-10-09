e1fd291adee21bd4e617c7b2d420e7464c48a4c6
Crazy Lama <lama23@dhcp-003125.iet.ntnu.no>
2017-10-09 13:38:50 +0200

    added saving file script

---
+++ Assets/scripts/SaveDataController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using System.Text;
+
+public class SaveDataController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		SaveItemInfo();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+	public void SaveItemInfo(){
+		string path = null;
+		path = "./data/saved_player.json";
+		// string str = ItemInfo.ToString();
+		string str = "{moin:hey}";
+		using (FileStream fs = new FileStream(path, FileMode.Create)){
+				using (StreamWriter writer = new StreamWriter(fs)){
+						writer.Write(str);
+				}
+		}
+		UnityEditor.AssetDatabase.Refresh ();
+}
+
+}
