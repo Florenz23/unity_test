ce169197c41387132086476168ec6ea6f07a8fc7
Crazy Lama <lama23@dhcp-003125.iet.ntnu.no>
2017-10-09 14:53:14 +0200

    trying new stream weiter

--- Assets/scripts/SaveDataController.cs
+++ Assets/scripts/SaveDataController.cs
@@ -24,16 +24,22 @@ public class SaveDataController : MonoBehaviour {
 	}
 	public void SaveItemInfo(){
 		string path = null;
+		string str = "";
 		path = "./Assets/data/saved_player.json";
 		MyClass myObject = new MyClass();
 		myObject.level = 1;
 		myObject.timeElapsed = 47.5f;
 		myObject.playerName = "Dr Charles Francis";
+		MyClass[] array = {myObject,myObject};
 
-		string str = JsonUtility.ToJson(myObject);
+		for (int i=0; i<array.Length; i++){
+			str = str + JsonUtility.ToJson(array[i]);
+		}
+
 		// string str = "{moin:hey}";
 		using (FileStream fs = new FileStream(path, FileMode.Create)){
 				using (StreamWriter writer = new StreamWriter(fs)){
+						writer.Write(str);
 						writer.Write(str);
 				}
 		}
