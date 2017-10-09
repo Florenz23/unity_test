a440dc5a9d5edd9352e87ab4843458b4a08ba360
Crazy Lama <lama23@dhcp-003125.iet.ntnu.no>
2017-10-09 14:18:02 +0200

    writing json to file works

--- Assets/scripts/SaveDataController.cs
+++ Assets/scripts/SaveDataController.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using System.IO;
-using System.Text;
+
 
 public class SaveDataController : MonoBehaviour {
 
+		public class MyClass{
+	    public int level;
+	    public float timeElapsed;
+	    public string playerName;
+	}
+
 	// Use this for initialization
-	public void Data(string speed, int time)
-    {
-        Speed = speed;
-        Time = time;
-    }
 	void Start () {
 		SaveItemInfo();
 	}
@@ -24,10 +25,13 @@ public class SaveDataController : MonoBehaviour {
 	public void SaveItemInfo(){
 		string path = null;
 		path = "./Assets/data/saved_player.json";
-		Data savedData = new Data("20",10);
+		MyClass myObject = new MyClass();
+		myObject.level = 1;
+		myObject.timeElapsed = 47.5f;
+		myObject.playerName = "Dr Charles Francis";
 
-		// string str = ItemInfo.ToString();
-		string str = "{moin:hey}";
+		string str = JsonUtility.ToJson(myObject);
+		// string str = "{moin:hey}";
 		using (FileStream fs = new FileStream(path, FileMode.Create)){
 				using (StreamWriter writer = new StreamWriter(fs)){
 						writer.Write(str);
